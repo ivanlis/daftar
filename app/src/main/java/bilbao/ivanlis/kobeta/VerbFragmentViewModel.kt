5600b5818019986715f544ab6e671aec18eb5308
Ivan Lysiuchenko <ivanlis.bilbao@gmail.com>
2019-07-04 20:56:59 +0200

    Change to MediatorLiveData in VebFragmentViewModel.

--- app/src/main/java/bilbao/ivanlis/kobeta/VerbFragmentViewModel.kt
+++ app/src/main/java/bilbao/ivanlis/kobeta/VerbFragmentViewModel.kt
@@ -1,6 +1,7 @@
 package bilbao.ivanlis.kobeta
 
 import android.app.Application
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
@@ -10,6 +11,38 @@ class VerbFragmentViewModel(application: Application, wordId: Long):
 
 
     val verbForms = repository.extractArabicVerbForms(wordId)
+
+    val verbTranslation = MediatorLiveData<String>()
+    val verbPast = MediatorLiveData<String>()
+    val verbNonpast = MediatorLiveData<String>()
+    val verbVerbalNoun = MediatorLiveData<String>()
+
+    init {
+        verbTranslation.addSource(verbForms) {
+            if (verbTranslation.value == null)
+                it?.let { vForms ->
+                    verbTranslation.value = vForms.translation
+                }
+        }
+        verbPast.addSource(verbForms) {
+            if (verbPast.value == null)
+                it?.let { vForms ->
+                    verbPast.value = vForms.pastForm
+                }
+        }
+        verbNonpast.addSource(verbForms) {
+            if (verbNonpast.value == null)
+                it?.let { vForms ->
+                    verbNonpast.value = vForms.nonpastForm
+                }
+        }
+        verbVerbalNoun.addSource(verbForms) {
+            if (verbVerbalNoun.value == null)
+                it?.let { vForms ->
+                    verbVerbalNoun.value = vForms.verbalNounForm
+                }
+        }
+    }
 
     override suspend fun executeSave(userInput: WordFormInput) {
         verbForms.value?.let { verbFormsVal ->
