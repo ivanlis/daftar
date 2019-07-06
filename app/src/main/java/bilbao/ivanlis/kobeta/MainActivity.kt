c79bf075a4d5ddf50854c3e3758a59a4d7adef91
Ivan Lysiuchenko <ivanlis.bilbao@gmail.com>
2019-07-06 14:47:46 +0200

    Start working on the main menu.

--- app/src/main/java/bilbao/ivanlis/kobeta/MainActivity.kt
+++ app/src/main/java/bilbao/ivanlis/kobeta/MainActivity.kt
@@ -1,7 +1,10 @@
 package bilbao.ivanlis.kobeta
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.notebook_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.notebook_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when(item.itemId) {
+        R.id.action_to_lessons_list -> {
+            //TODO: navigate to the lessons list fragment
+            //supportFragmentManager.beginTransaction()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
