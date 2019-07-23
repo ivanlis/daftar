d8a0a992c3a035bdad1817304453824e8d1f0493
Ivan Lysiuchenko <ivanlis.bilbao@gmail.com>
2019-07-23 19:35:26 +0200

    Control element visibility on EvaluationFragment.

--- app/src/main/java/bilbao/ivanlis/daftar/EvaluationViewModel.kt
+++ app/src/main/java/bilbao/ivanlis/daftar/EvaluationViewModel.kt
@@ -1,7 +1,11 @@
 package bilbao.ivanlis.daftar
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.*
+import bilbao.ivanlis.daftar.constants.POS_NOUN
+import bilbao.ivanlis.daftar.constants.POS_PARTICLE
+import bilbao.ivanlis.daftar.constants.POS_VERB
 import bilbao.ivanlis.daftar.database.TrainingProcess
 import java.lang.IllegalArgumentException
 
@@ -14,8 +18,23 @@ class EvaluationViewModel(application: Application, val trueValues: WordFormInpu
     val navigateToNextExercise: LiveData<Boolean>
         get() = _navigateToNextExercise
 
+    private val _verbVisible = MutableLiveData<Int>()
+    val verbVisible: LiveData<Int>
+        get() = _verbVisible
+
+    private val _nounVisible = MutableLiveData<Int>()
+    val nounVisible: LiveData<Int>
+        get() = _nounVisible
+
+    private val _particleVisible = MutableLiveData<Int>()
+    val particleVisible: LiveData<Int>
+        get() = _particleVisible
+
     init {
         _navigateToNextExercise.value = false
+        _verbVisible.value = posToVerbVisibility(trueValues.posChosen)
+        _nounVisible.value = posToNounVisibility(trueValues.posChosen)
+        _particleVisible.value = posToParticleVisibility(trueValues.posChosen)
     }
 
     fun onNavigateToNextExercise() {
@@ -25,6 +44,22 @@ class EvaluationViewModel(application: Application, val trueValues: WordFormInpu
     fun onNavigateToNextExerciseComplete() {
         _navigateToNextExercise.value = false
     }
+
+    fun posToVerbVisibility(pos: String) =
+        when(pos) {
+            POS_VERB -> View.VISIBLE
+            else -> View.GONE
+        }
+    fun posToNounVisibility(pos: String) =
+        when(pos) {
+            POS_NOUN -> View.VISIBLE
+            else -> View.GONE
+        }
+    fun posToParticleVisibility(pos: String) =
+        when(pos) {
+            POS_PARTICLE -> View.VISIBLE
+            else -> View.GONE
+        }
 }
 
 class EvaluationViewModelFactory(private val application: Application,
